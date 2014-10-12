e45b80de9c49870f8e960af9c794c166e540e49c
Cloudpaw <td1336@gmail.com>
2014-10-12 02:13:42 -0400

    Added Update Searcher

--- Releases/2.0pr/kernal.lua
+++ Releases/2.0pr/kernal.lua
@@ -24,6 +24,13 @@ end
  
  
 local function Boot()
+local updater = dofile("lib/updater.lua")
+
+local function success()
+	print("Update Found!")
+end
+updater.new() -- this will check for any updates for the updater
+updater.update(success, "Cloudpaw", "CP-DOS") -- first argument is the function to be executed in case of an update second is the username and last is the repostery
 term.clear()
 textutils.slowPrint("Booting CP-DOS")
 bar = gui.createBar( "bar" ) --#This creates a new bar with the name
---
+++ Releases/2.0pr/lib/updater.lua
@@ -0,0 +1,84 @@
+local JSON = dofile("lib/dkjson.lua")
+
+local updater = {}
+
+local function getLocal(v)
+	if v then
+		if fs.exists("lib/.version") then
+			local file = fs.open("lib/.version", "r")
+			local ver = file.readAll()
+			file.close()
+			return tonumber(ver)
+		else
+			return 0
+		end
+	else
+		if fs.exists("lib/.dlversion") then
+			local file = fs.open("lib/.dlversion", "r")
+			local ver = file.readAll()
+			file.close()
+			return tonumber(ver)
+		else
+			return 0
+		end	
+	end
+end
+
+local function getOnline(user, repo)
+	local size = 0
+	local query = "https://api.github.com/repos/"..user.."/"..repo.."/commits"
+	local request = http.get(query).readAll()
+	local data = JSON.decode(request)
+	size = size + #data
+	local sha = data[#data].sha
+	repeat
+		local query = "https://api.github.com/repos/"..user.."/"..repo.."/commits?sha="..sha
+		local request = http.get(query).readAll()
+		local data = JSON.decode(request)
+		if #data ~= 1 then
+			size = size + #data
+			size = size - 1
+		end
+		sha = data[#data].sha
+	until #data == 1
+	return size
+end
+
+local function setLocal(ver, v)
+	if v then
+		local file = fs.open("lib/.version", "w")
+		file.write(ver)
+		file.close()
+	else
+		local file = fs.open("lib/.dlversion", "w")
+		file.write(ver)
+		file.close()
+	end
+end
+
+function updater.update(funcToRun, user, repo)
+	local onl = getOnline(user, repo)
+	local loc = getLocal(true)
+	if onl > loc then
+		setLocal(onl, true)
+		funcToRun()
+	end
+end
+
+function updater.new()
+	local onl = getOnline("Kolpa", "cc-update-checker")
+	local loc = getLocal(false)
+	if onl > loc then
+		print("a new version of the updater api is downloading now")
+		local new = http.get("https://raw.github.com/Kolpa/cc-update-checker/master/lib/updater.lua").readAll()
+		local file = fs.open("lib/updater.lua", "w")
+		file.write(new)
+		file.close()
+		setLocal(onl, false)
+		print("Restarting now")
+		sleep(1)
+		shell.run(shell.getRunningProgram())
+	end
+end
+
+return updater
